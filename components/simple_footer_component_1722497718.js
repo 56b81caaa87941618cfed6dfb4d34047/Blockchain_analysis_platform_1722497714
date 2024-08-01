b49051cca49dd70705bfa38602587da6f0d1ca0c
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 13:05:29 +0530

    Placeholder text replacement

--- components/simple_footer_component_1722497718.js
+++ components/simple_footer_component_1722497718.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722497718", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            BlocksEye - Blockchain Analytics Platform
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the curve with BlocksEye. Our cutting-edge platform provides real-time analysis and insights into the latest blockchain data, transactions, and trends. Powered by AI and machine learning, we help you make informed decisions in the dynamic world of decentralized technologies.
                     </div>
                 </div>
             </div>
